cfec0f2b8767624dc617ae2fdb1f6bf25d78e1c1
Jin Igarashi <igarashi.jin@water-gis.com>
2022-10-16 14:39:16 +0100

    changed config to nawassco

--- src/config.ts
+++ src/config.ts
@@ -1,120 +1,114 @@
 import type { Config } from './lib/types';
 
 export const config: Config = {
-	title: 'GIS for Water',
-	url: 'https://water-gis.com',
-	logo: 'https://raw.githubusercontent.com/watergis/logo/main/logo.svg',
-	// basePath: process.env.NODE_ENV === 'production' ? '/sveltekit-watergis-template' : '',
-	basePath: '',
+	title: 'Water Supply Map for Nanyuki Water',
+	url: 'https://nawasco.co.ke',
+	logo: '/favicon.png',
+	basePath: process.env.NODE_ENV === 'production' ? '/watergis' : '',
+	// basePath: '',
 	// change attribution to yours
-	attribution: '©NARWASSCO',
+	attribution: '©Nanyuki Water and Serwerage Co., Ltd.',
 	// change stylefiles URLs to yours
 	styles: [
+		// {
+		// 	title: 'Satellite(water)',
+		// 	uri: `https://nawasco.github.io/mapbox-stylefiles/satellite/style.json`
+		// },
 		{
-			title: 'UNVT Water (OSM)',
-			uri: `https://narwassco.github.io/mapbox-stylefiles/unvt/style.json`
+			title: 'UNVT Buildings(water)',
+			uri: `https://nawasco.github.io/mapbox-stylefiles/unvt/style-buildings-water.json`
 		},
+		// {
+		// 	title: 'Street(water)',
+		// 	uri: `https://nawasco.github.io/mapbox-stylefiles/street/style.json`
+		// },
+		// {
+		// 	title: 'Satellite(sewer)',
+		// 	uri: `https://nawasco.github.io/mapbox-stylefiles/satellite/style_sewer.json`
+		// },
 		{
-			title: 'UNVT Water (Building)',
-			uri: `https://narwassco.github.io/mapbox-stylefiles/unvt/style-buildings.json`
+			title: 'UNVT Buildings(sewer)',
+			uri: `https://nawasco.github.io/mapbox-stylefiles/unvt/style-buildings-sewer.json`
 		},
+		// {
+		// 	title: 'Street(sewer)',
+		// 	uri: `https://nawasco.github.io/mapbox-stylefiles/street/style_sewer.json`
+		// },
 		{
-			title: 'Satellite Water',
-			uri: `https://narwassco.github.io/mapbox-stylefiles/unvt/style-aerial.json`
-		},
-		{
-			title: 'UNVT Sewer',
-			uri: `https://narwassco.github.io/mapbox-stylefiles/unvt/style-sewer.json`
-		},
-		{
-			title: 'Satellite Sewer',
-			uri: `https://narwassco.github.io/mapbox-stylefiles/unvt/style-aerial-sewer.json`
+			title: 'UN Vector Tile',
+			uri: `https://nawasco.github.io/mapbox-stylefiles/unvt/style.json`
 		}
 	],
 	// change initial location and zoom level to yours
-	center: { lng: 35.87063, lat: -1.08551 },
+	center: [37.119039, -0.024493],
 	zoom: 13,
 	// you can put your geojson file for searching functions
 	search: {
-		url: 'https://narwassco.github.io/vt/meter.geojson',
-		target: ['connno', 'serialno', 'village'],
+		url: 'https://nawasco.github.io/vt/meter.geojson',
+		target: ['accountno', 'serialno'],
 		format: (p) => {
 			const label: string[] = [];
-			const targets = ['connno', 'serialno', 'village'];
+			const targets = ['accountno', 'serialno', 'category'];
 			targets.forEach((target) => {
-				if (p[target]) label.push(p[target]);
+				if (p[target]) {
+					let text = p[target];
+					switch (target) {
+						case 'accountno':
+							text = `A/C: ${text}`;
+							break;
+						case 'serialno':
+							text = `S/N: ${text}`;
+							break;
+						default:
+							break;
+					}
+					label.push(text);
+				}
 			});
 			return label.length > 0 ? label.join(', ') : '';
 		},
 		place_type: ['meter'],
-		placeholder: 'Search CONN NO or S/N',
-		zoom: 17
+		placeholder: 'Account No or S/N',
+		zoom: 16
 	},
 	// please specify layers' name for showing popup with attributes table.
 	popup: {
 		target: [
-			'meter',
-			'flow meter',
-			'valve',
-			'washout',
-			'firehydrant',
-			'tank',
 			'pipeline',
-			'pg-building',
-			'sewer-connection',
-			'sewer_commercial',
-			'sewer_institution',
-			'sewer_public_toilet',
-			'sewer_pipeline',
-			'sewer_treatment_plant',
-			'manhole'
-		]
-	},
-	// please specify your covered area if you have multipul locations to do waterworks
-	areaSwitcher: {
-		areas: [
-			{ title: 'Narok', latlng: [35.87063, -1.08551], zoom: 14 },
-			{ title: "Ololulung'a", latlng: [35.65072, -1.0085], zoom: 13 },
-			{ title: 'Kilgoris', latlng: [34.87533, -1.00278], zoom: 14 },
-			{ title: 'Suswa', latlng: [36.33078, -1.05307], zoom: 13 }
+			'sewerline',
+			'meter',
+			'kiosks',
+			'borehole',
+			'tank',
+			'booster-pump',
+			'chambers',
+			'manholes',
+			'pg-building'
 		]
 	},
 	// please specify layer name for showing in legend panel.
 	legend: {
 		targets: {
+			cadastral: 'Cadastral',
+			'cadastral-annotation': 'Cadastral Label',
+			blocks: 'Blocks',
+			'blocks-annotation': 'Blocks Label',
+			waterworks: 'Waterworks',
 			pipeline: 'Pipeline',
 			pipeline_annotation: 'Pipeline Label',
-			meter: 'Water Meter',
-			'flow meter': 'Flow Meter',
-			valve: 'Valve',
-			firehydrant: 'Fire Hydrant',
-			washout: 'Washout',
+			sewerline: 'Sewer Pipeline',
+			sewerline_annotation: 'Sewer Pipeline Label',
+			meter: 'Consumer Meter',
+			kiosks: 'Water Kiosk',
+			borehole: 'Borehole',
 			tank: 'Tank',
-			tank_annotation: 'Tank Label',
-			wtp: 'WTP',
-			wtp_annotation: 'WTP Label',
-			intake: 'Intake',
-			intake_annotation: 'Intake Label',
-			parcels: 'Parcels',
-			parcels_annotation: 'Parcels Label',
-			village: 'Village',
-			village_annotation: 'Village Label',
-			dma: 'DMA',
-			'dma-annotation': 'DMA Label',
-			'contour-line': 'Countour',
-			'contour-label': 'Contour Label',
-			hillshade: 'Hillshade',
-			sewer_connection: 'Households (Sewer)',
-			sewer_commercial: 'Commecial (Sewer)',
-			sewer_institution: 'Institution (Sewer)',
-			sewer_public_toilet: 'Public Toilet (Sewer)',
-			manhole: 'Manhole',
-			sewer_pipeline: 'Sewer pipeline',
-			sewer_treatment_plant: 'Wastewater treatment plant'
+			'booster-pump': 'Booster Pump',
+			chambers: 'Chamber',
+			manholes: 'Manhole'
 		}
 	},
 	elevation: {
-		url: 'https://narwassco.github.io/narok-terrain/tiles/{z}/{x}/{y}.png',
+		url: 'https://nawasco.github.io/ke-central-terrain/tiles/{z}/{x}/{y}.png',
 		options: {
 			tileSize: 512,
 			font: ['Roboto Medium'],
@@ -160,9 +154,9 @@ export const config: Config = {
 				iconSize: 1
 			}
 		}
-	},
-	terrain: {
-		source: 'narok-dem',
-		exaggeration: 1
 	}
+	// terrain: {
+	// 	source: 'narok-dem',
+	// 	exaggeration: 1
+	// }
 };
--- svelte.config.js
+++ svelte.config.js
@@ -16,8 +16,8 @@ const config = {
 		}),
 		paths: {
 			// YOUR github repository name
-			// base: process.env.NODE_ENV === 'production' ? '/sveltekit-watergis-template' : ''
-			base: ''
+			base: process.env.NODE_ENV === 'production' ? '/watergis' : ''
+			// base: ''
 		}
 	}
 };
