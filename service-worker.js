d5543bd11d29f906d1a141e22290f6f853632b58
JinIgarashi <igarashi.jin@water-gis.com>
2022-11-25 22:13:25 +0000

    Deploying to gh-pages from @ nawasco/watergis@83b80415d226e9ce0ec60703ddf98e15a07073b1 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,23 @@
 const l = [
-  "/watergis/_app/immutable/assets/fa-solid-900-d27bc752.woff2",
-  "/watergis/_app/immutable/assets/fa-solid-900-6d53c706.ttf",
-  "/watergis/_app/immutable/start-d06c0b46.js",
+  "/watergis/_app/immutable/start-08aa4ed6.js",
   "/watergis/_app/immutable/components/layout.svelte-59b0d820.js",
   "/watergis/_app/immutable/components/error.svelte-a2418c2d.js",
-  "/watergis/_app/immutable/components/pages/_page.svelte-484f3c88.js",
+  "/watergis/_app/immutable/components/pages/_page.svelte-21b0d0b6.js",
   "/watergis/_app/immutable/modules/pages/_page.ts-f4c35b8f.js",
   "/watergis/_app/immutable/chunks/singletons-687127a0.js",
   "/watergis/_app/immutable/chunks/index-73d65c13.js",
   "/watergis/_app/immutable/chunks/index-aae2ee95.js",
   "/watergis/_app/immutable/chunks/preload-helper-9b728935.js",
-  "/watergis/_app/immutable/chunks/_page-424f9762.js",
-  "/watergis/_app/immutable/assets/_page-e9e3f33b.css",
+  "/watergis/_app/immutable/chunks/_page-336da03b.js",
+  "/watergis/_app/immutable/assets/_page-123f531b.css",
   "/watergis/_app/immutable/chunks/_page-c82b235a.js",
   "/watergis/_app/immutable/chunks/0-e732bbe7.js",
   "/watergis/_app/immutable/chunks/1-02568370.js",
-  "/watergis/_app/immutable/chunks/2-958c1390.js",
+  "/watergis/_app/immutable/chunks/2-a75f805b.js",
   "/watergis/_app/immutable/chunks/html2canvas.esm-fb42d204.js",
   "/watergis/_app/immutable/chunks/purify.es-88e864ff.js",
-  "/watergis/_app/immutable/chunks/index.es-a0943890.js"
-], m = [
+  "/watergis/_app/immutable/chunks/index.es-71d10b7d.js"
+], g = [
   "/watergis/.nojekyll",
   "/watergis/assets/icons/icon-128x128.png",
   "/watergis/assets/icons/icon-144x144.png",
@@ -33,10 +31,10 @@ const l = [
   "/watergis/favicon.png",
   "/watergis/manifest.webmanifest",
   "/watergis/robots.txt"
-], n = "1669412997218", a = self, o = `cache${n}`, p = l.concat(m), g = new Set(p);
+], c = "1669414367427", a = self, o = `cache${c}`, r = l.concat(g), m = new Set(r);
 a.addEventListener("install", (s) => {
   s.waitUntil(
-    caches.open(o).then((e) => e.addAll(p)).then(() => {
+    caches.open(o).then((e) => e.addAll(r)).then(() => {
       a.skipWaiting();
     })
   );
@@ -51,7 +49,7 @@ a.addEventListener("activate", (s) => {
   );
 });
 async function h(s) {
-  const e = await caches.open(`offline${n}`);
+  const e = await caches.open(`offline${c}`);
   try {
     const t = await fetch(s);
     return e.put(s, t.clone()), t;
@@ -65,8 +63,8 @@ async function h(s) {
 a.addEventListener("fetch", (s) => {
   if (s.request.method !== "GET" || s.request.headers.has("range"))
     return;
-  const e = new URL(s.request.url), t = e.protocol.startsWith("http"), i = e.hostname === self.location.hostname && e.port !== self.location.port, c = e.host === self.location.host && g.has(e.pathname), r = s.request.cache === "only-if-cached" && !c;
-  t && !i && !r && s.respondWith(
-    (async () => c && await caches.match(s.request) || h(s.request))()
+  const e = new URL(s.request.url), t = e.protocol.startsWith("http"), i = e.hostname === self.location.hostname && e.port !== self.location.port, n = e.host === self.location.host && m.has(e.pathname), p = s.request.cache === "only-if-cached" && !n;
+  t && !i && !p && s.respondWith(
+    (async () => n && await caches.match(s.request) || h(s.request))()
   );
 });
