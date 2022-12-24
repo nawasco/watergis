df089d5d633de4e70703fd1d879048bf2281a07f
JinIgarashi <igarashi.jin@water-gis.com>
2022-12-24 17:00:13 +0000

    Deploying to gh-pages from @ nawasco/watergis@e0b49dd29cdbf3ba4d8ff1c5ef9efd343d4569ac 🚀

---
+++ _app/immutable/components/pages/_page.svelte-36a7bc85.js
@@ -0,0 +1,1 @@
+import"../../chunks/preload-helper-41c905a7.js";import"../../chunks/index-d23b2ecc.js";import{P as p}from"../../chunks/_page-ed277b53.js";export{p as default};
--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,23 @@
 const m = [
-  "/_app/immutable/assets/_page-68aaa35b.css",
-  "/_app/immutable/modules/pages/_page.ts-6a8cdd0a.js",
+  "/_app/immutable/assets/_page-3795d1c4.css",
+  "/_app/immutable/chunks/0-016a1d45.js",
+  "/_app/immutable/chunks/singletons-d7293337.js",
+  "/_app/immutable/chunks/index-d23b2ecc.js",
+  "/_app/immutable/chunks/preload-helper-41c905a7.js",
+  "/_app/immutable/components/error.svelte-1d9f4709.js",
+  "/_app/immutable/chunks/2-c69d75a2.js",
+  "/_app/immutable/chunks/1-8d9436e2.js",
   "/_app/immutable/chunks/index-95d721c5.js",
   "/_app/immutable/chunks/_page-7e4796dc.js",
-  "/_app/immutable/chunks/index-71eac763.js",
-  "/_app/immutable/components/pages/_page.svelte-480f6154.js",
-  "/_app/immutable/chunks/purify.es-075a2866.js",
-  "/_app/immutable/chunks/preload-helper-41c905a7.js",
-  "/_app/immutable/chunks/0-016a1d45.js",
-  "/_app/immutable/chunks/1-8d9436e2.js",
-  "/_app/immutable/chunks/2-b9731ab0.js",
-  "/_app/immutable/chunks/singletons-d7293337.js",
-  "/_app/immutable/start-7e64f86d.js",
   "/_app/immutable/components/layout.svelte-99529342.js",
-  "/_app/immutable/components/error.svelte-1d9f4709.js",
-  "/_app/immutable/chunks/index-d23b2ecc.js",
-  "/_app/immutable/chunks/index.es-1ee718b8.js",
+  "/_app/immutable/modules/pages/_page.ts-6a8cdd0a.js",
+  "/_app/immutable/chunks/purify.es-075a2866.js",
+  "/_app/immutable/components/pages/_page.svelte-36a7bc85.js",
+  "/_app/immutable/start-6955e009.js",
+  "/_app/immutable/chunks/index-71eac763.js",
   "/_app/immutable/chunks/html2canvas.esm-0a33ed42.js",
-  "/_app/immutable/chunks/_page-20446856.js"
+  "/_app/immutable/chunks/index.es-01e35d2c.js",
+  "/_app/immutable/chunks/_page-ed277b53.js"
 ], h = [
   "/.nojekyll",
   "/assets/icons/icon-128x128.png",
@@ -33,40 +33,40 @@ const m = [
   "/favicon.png",
   "/manifest.webmanifest",
   "/robots.txt"
-], i = "1671793964136", t = self, o = `cache${i}`, p = m.concat(h), u = new Set(p);
-t.addEventListener("install", (s) => {
+], i = "1671901165431", a = self, o = `cache${i}`, p = m.concat(h), u = new Set(p);
+a.addEventListener("install", (s) => {
   s.waitUntil(
     caches.open(o).then((e) => e.addAll(p)).then(() => {
-      t.skipWaiting();
+      a.skipWaiting();
     })
   );
 });
-t.addEventListener("activate", (s) => {
+a.addEventListener("activate", (s) => {
   s.waitUntil(
     caches.keys().then(async (e) => {
-      for (const a of e)
-        a !== o && await caches.delete(a);
-      t.clients.claim();
+      for (const t of e)
+        t !== o && await caches.delete(t);
+      a.clients.claim();
     })
   );
 });
 async function r(s) {
   const e = await caches.open(`offline${i}`);
   try {
-    const a = await fetch(s);
-    return e.put(s, a.clone()), a;
-  } catch (a) {
-    const n = await e.match(s);
-    if (n)
-      return n;
-    throw a;
+    const t = await fetch(s);
+    return e.put(s, t.clone()), t;
+  } catch (t) {
+    const c = await e.match(s);
+    if (c)
+      return c;
+    throw t;
   }
 }
-t.addEventListener("fetch", (s) => {
+a.addEventListener("fetch", (s) => {
   if (s.request.method !== "GET" || s.request.headers.has("range"))
     return;
-  const e = new URL(s.request.url), a = e.protocol.startsWith("http"), n = e.hostname === self.location.hostname && e.port !== self.location.port, c = e.host === self.location.host && u.has(e.pathname), l = s.request.cache === "only-if-cached" && !c;
-  a && !n && !l && s.respondWith(
-    (async () => c && await caches.match(s.request) || r(s.request))()
+  const e = new URL(s.request.url), t = e.protocol.startsWith("http"), c = e.hostname === self.location.hostname && e.port !== self.location.port, n = e.host === self.location.host && u.has(e.pathname), l = s.request.cache === "only-if-cached" && !n;
+  t && !c && !l && s.respondWith(
+    (async () => n && await caches.match(s.request) || r(s.request))()
   );
 });
