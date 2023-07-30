3b11fbb9aa3262b6d5e6551a7f550daa964c8d7e
JinIgarashi <igarashi.jin@water-gis.com>
2023-07-30 12:21:34 +0000

    Deploying to gh-pages from @ nawasco/watergis@a8be73c59e9e0876b146474ad4e259ed2de27b73 🚀

--- _app/immutable/nodes/2.3bf7973f.js
+++
@@ -1,1 +0,0 @@
-import{P as e,a as n}from"../chunks/2.5c459728.js";export{e as component,n as universal};
--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,18 @@
 const s = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), r = [
-  s + "/_app/immutable/entry/app.ac4914a5.js",
+  s + "/_app/immutable/entry/app.fd438aaa.js",
   s + "/_app/immutable/nodes/0.431c0a10.js",
-  s + "/_app/immutable/nodes/1.bf290ec9.js",
-  s + "/_app/immutable/nodes/2.3bf7973f.js",
-  s + "/_app/immutable/assets/2.b83b1af7.css",
-  s + "/_app/immutable/chunks/2.5c459728.js",
+  s + "/_app/immutable/nodes/1.d84a4b92.js",
+  s + "/_app/immutable/nodes/2.eb824fa1.js",
+  s + "/_app/immutable/assets/2.8531a707.css",
+  s + "/_app/immutable/chunks/2.36249c8f.js",
+  s + "/_app/immutable/chunks/index.2f4651e6.js",
   s + "/_app/immutable/chunks/index.7bbd4199.js",
-  s + "/_app/immutable/chunks/index.7ef8d12a.js",
   s + "/_app/immutable/chunks/index.89c07c5d.js",
   s + "/_app/immutable/chunks/preload-helper.cf010ec4.js",
   s + "/_app/immutable/chunks/scheduler.5d7259d7.js",
-  s + "/_app/immutable/chunks/singletons.533c5343.js",
-  s + "/_app/immutable/entry/start.35cbddb8.js",
-  s + "/_app/immutable/chunks/index.es.4487c546.js",
+  s + "/_app/immutable/chunks/singletons.115a8a35.js",
+  s + "/_app/immutable/entry/start.7d0a6e46.js",
+  s + "/_app/immutable/chunks/index.es.4ff2b2c7.js",
   s + "/_app/immutable/chunks/purify.es.cf254a40.js",
   s + "/_app/immutable/chunks/html2canvas.esm.e0a7d97b.js"
 ], m = [
@@ -30,20 +30,20 @@ const s = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), r
   s + "/favicon.png",
   s + "/manifest.webmanifest",
   s + "/robots.txt"
-], o = "1690479359514", c = self, p = `cache${o}`, h = r.concat(m), u = new Set(h);
-c.addEventListener("install", (e) => {
+], o = "1690719631005", n = self, p = `cache${o}`, h = r.concat(m), u = new Set(h);
+n.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open(p).then((t) => t.addAll(h)).then(() => {
-      c.skipWaiting();
+      n.skipWaiting();
     })
   );
 });
-c.addEventListener("activate", (e) => {
+n.addEventListener("activate", (e) => {
   e.waitUntil(
     caches.keys().then(async (t) => {
       for (const a of t)
         a !== p && await caches.delete(a);
-      c.clients.claim();
+      n.clients.claim();
     })
   );
 });
@@ -53,17 +53,17 @@ async function d(e) {
     const a = await fetch(e);
     return t.put(e, a.clone()), a;
   } catch (a) {
-    const n = await t.match(e);
-    if (n)
-      return n;
+    const c = await t.match(e);
+    if (c)
+      return c;
     throw a;
   }
 }
-c.addEventListener("fetch", (e) => {
+n.addEventListener("fetch", (e) => {
   if (e.request.method !== "GET" || e.request.headers.has("range"))
     return;
-  const t = new URL(e.request.url), a = t.protocol.startsWith("http"), n = t.hostname === self.location.hostname && t.port !== self.location.port, i = t.host === self.location.host && u.has(t.pathname), l = e.request.cache === "only-if-cached" && !i;
-  a && !n && !l && e.respondWith(
+  const t = new URL(e.request.url), a = t.protocol.startsWith("http"), c = t.hostname === self.location.hostname && t.port !== self.location.port, i = t.host === self.location.host && u.has(t.pathname), l = e.request.cache === "only-if-cached" && !i;
+  a && !c && !l && e.respondWith(
     (async () => i && await caches.match(e.request) || d(e.request))()
   );
 });
